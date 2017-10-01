7adf3b3d725dd45191885d6e6d5f9db8bd658ae8
Hoax017 <m.aymeric@gmail.com>
2017-10-01 13:39:17 +0000

    Fonctionne

--- Test_JS/test.js
+++ Test_JS/test.js
@@ -1,37 +1,50 @@
 function texte1() {
 	console.log('Text1');
-	$('.valeur1').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte1);
-	$('.valeur2').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte1);
-	$('.valeur3').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte1);
+	$('.valeur1').fadeTo(3000, 1.0).fadeTo(3000, 0.0);
+	$('.valeur2').fadeTo(3000, 1.0).fadeTo(3000, 0.0);
+	$('.valeur3').fadeTo(3000, 1.0).fadeTo(3000, 0.0);
 };
 function texte2() {
-	console.log('Text2');
-	$('.valeur4').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte2);
-	$('.valeur5').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte2);
-	$('.valeur6').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte2);
+	$('.valeur4').fadeTo(3000, 1.0).fadeTo(3000, 0.0);
+	$('.valeur5').fadeTo(3000, 1.0).fadeTo(3000, 0.0);
+	$('.valeur6').fadeTo(3000, 1.0).fadeTo(3000, 0.0);
+
+};
+function texte3() {
+	$('.valeur7').fadeTo(3000, 1.0).fadeTo(3000, 0.0);
+	$('.valeur8').fadeTo(3000, 1.0).fadeTo(3000, 0.0);
+	$('.valeur9').fadeTo(3000, 1.0).fadeTo(3000, 0.0);
 
 };
 
 
 var funct = "texte1";
+$('.valeur1').fadeTo(1, 0.0);
+$('.valeur2').fadeTo(1, 0.0);
+$('.valeur3').fadeTo(1, 0.0);
+$('.valeur4').fadeTo(1, 0.0);
+$('.valeur5').fadeTo(1, 0.0);
+$('.valeur6').fadeTo(1, 0.0);
+$('.valeur7').fadeTo(1, 0.0);
+$('.valeur8').fadeTo(1, 0.0);
+$('.valeur9').fadeTo(1, 0.0);
 
 var MaBoucle = setInterval(function() {
-	// arret
-	// clearInterval(MaBoucle);
-
-
-	console.log("Execution", funct);
-
 	if (funct == "texte1")
 	{
 		texte1();
-		funct = "texte2";
+		funct ="texte2";
+	}
+	else if (funct == "texte2")
+	{
+		texte2();
+		funct ="texte3";
 	}
 	else
 	{
-		texte2();
+		texte3();
 		funct = "texte1";
 	}
 
 
-}, 19000); // 7s
+}, 7000); // 7s
