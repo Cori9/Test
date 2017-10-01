59846c796618e3447bfea063b8a0dabb4ad3f18b
Hoax017 <m.aymeric@gmail.com>
2017-10-01 13:17:33 +0000

    log

--- Test_JS/test.js
+++ Test_JS/test.js
@@ -1,31 +1,37 @@
+function texte1() {
+	console.log('Text1');
+	$('.valeur1').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte1);
+	$('.valeur2').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte1);
+	$('.valeur3').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte1);
+};
+function texte2() {
+	console.log('Text2');
+	$('.valeur4').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte2);
+	$('.valeur5').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte2);
+	$('.valeur6').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte2);
+
+};
 
 
+var funct = "texte1";
+
+var MaBoucle = setInterval(function() {
+	// arret
+	// clearInterval(MaBoucle);
 
 
+	console.log("Execution", funct);
+
+	if (funct == "texte1")
+	{
+		texte1();
+		funct = "texte2";
+	}
+	else
+	{
+		texte2();
+		funct = "texte1";
+	}
 
 
-
-
-
-
-
-
-
-
-
-
-// // $(function boucle(){
-  $(function texte1() {
-    $('.valeur1').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte1);
-    $('.valeur2').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte1);
-    $('.valeur3').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte1); });
-    $(function texte2() {
-    $('.valeur4').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte2);
-    $('.valeur5').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte2);
-    $('.valeur6').fadeTo(3000, 1.0).fadeTo(3000, 0.0, texte2); });
-
-    // $('.valeur7').delay(4500).fadeTo(2500, 1.0).delay(5000).fadeTo(2500, 0.0, texte).delay(7000);
-    // $('.valeur8').delay(7000).fadeTo(2500, 1.0).delay(5000).fadeTo(2500, 0.0, texte).delay(8000);
-    // $('.valeur9').delay(6000).fadeTo(2500, 1.0).delay(5000).fadeTo(2500, 0.0, texte).delay(9000);
-    // });
-// });
+}, 19000); // 7s
